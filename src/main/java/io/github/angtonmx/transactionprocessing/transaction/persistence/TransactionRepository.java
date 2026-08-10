10061fbdb298d2d3c4488290eb1a85b4a506729b
Ivan Ramirez <ragrerit19@gmail.com>
2026-08-10 08:55:24 -0600

    feat: add transaction persistence

---
+++ src/main/java/io/github/angtonmx/transactionprocessing/transaction/persistence/TransactionRepository.java
@@ -0,0 +1,9 @@
+package io.github.angtonmx.transactionprocessing.transaction.persistence;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository
+        extends JpaRepository<TransactionEntity, UUID> {
+}
